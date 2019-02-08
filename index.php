06e14e6873da7119acb1913f740d8bb43dda7f49
Jimmy Latour <lelabodudev@gmail.com>
2019-02-08 18:18:42 +0100

    Update

--- index.php
+++ index.php
@@ -6,19 +6,29 @@ use RestService\Server;
 
 Server::create('/')
     ->addGetRoute('players', function(){
-        return array(
-            array(
-                'name' => 'Ninja',
-                'kd' => 10.79,
-                'kills' => 98680,
-                'wins' => 5162,
-                'matches' => 14309,
-                'winrate' => 36.08,
-                'score' => 5506866,
-            )
-        );
+      return json_decode( file_get_contents( './Data/players.json'), true );
     })
     ->addPostRoute('update', function($pseudo) {
-      return $pseudo;
+      $content = json_decode( file_get_contents( './Data/players.json'), true );
+
+      if ( ! isset( $content[ $pseudo ] ) ) {
+        $content[ $pseudo ] = array(
+          "name" => $pseudo,
+          "kd" => 10.79,
+          "kills" => 98680,
+          "wins" => 5162,
+          "matches" => 14309,
+          "winrate" => 36.08,
+          "score" => 5506866
+        );
+      }
+
+      $f = fopen('./Data/players.json', 'w+');
+
+      fputs($f, json_encode( $content )); // On écrit le nouveau nombre de pages vues
+
+      fclose($f);
+
+      return true;
     })
 ->run();
